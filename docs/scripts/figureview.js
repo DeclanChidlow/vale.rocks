1cfb6109385d701183762aeb0750e3e46605ee66
Declan Chidlow <git.rh92c@8shield.net>
2024-06-28 20:05:33 +0800

    Move from Prism to Shiki for syntax highlighing

---
+++ docs/scripts/figureview.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const images = document.querySelectorAll("figure img"),
+		modal = document.getElementById("fullscreenModal"),
+		modalImg = document.getElementById("fullscreenImg"),
+		captionText = document.getElementById("caption"),
+		altText = document.getElementById("altText"),
+		closeButton = document.getElementById("close");
+
+	Array.from(images).forEach((img) => {
+		img.style.cursor = "zoom-in";
+		img.addEventListener("click", () => openFullscreen(img));
+	});
+
+	closeButton.addEventListener("click", closeFullscreen);
+
+	modal.addEventListener("click", (event) => {
+		if (event.target === modal) {
+			closeFullscreen();
+		}
+	});
+
+	function openFullscreen(imgElement) {
+		modal.style.display = "block";
+		modalImg.src = imgElement.currentSrc || imgElement.src;
+		altText.textContent = imgElement.alt;
+
+		let caption = imgElement.nextElementSibling;
+		if (caption && caption.tagName.toLowerCase() === "figcaption") {
+			captionText.textContent = caption.textContent;
+		}
+	}
+
+	function closeFullscreen() {
+		modal.style.display = "none";
+	}
+});
