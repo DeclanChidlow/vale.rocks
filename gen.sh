4b81aeb8b65c55f4c6309554e44da2904de75e52
Declan Chidlow <git.rh92c@8shield.net>
2024-07-14 11:59:54 +0800

    Format gen script

--- gen.sh
+++ gen.sh
@@ -2,41 +2,41 @@
 
 # Generate standard pages
 pages=(
-    "input/pages/index index.html"
-    "input/pages/posts posts.html"
-    "input/pages/portfolio portfolio.html"
-    "input/pages/contact contact.html"
-    "input/pages/support support.html"
-    "input/pages/services services.html"
+  "input/pages/index index.html"
+  "input/pages/posts posts.html"
+  "input/pages/portfolio portfolio.html"
+  "input/pages/contact contact.html"
+  "input/pages/support support.html"
+	"input/pages/services services.html"
 	"input/pages/404 404.html"
 )
 
 for page_input in "${pages[@]}"; do
-    page="${page_input%% *}"
-    output="${page_input#* }"
-    adduce -c "$page" -n "$output" -o docs
+  page="${page_input%% *}"
+  output="${page_input#* }"
+	adduce -c "$page" -n "$output" -o docs
 done
 
 # Generate posts
 cd input/posts || exit
 post_feeds=(
-    "Welcome"
-    "School-Internet"
-    "LibreOffice-Setup"
-    "Halo-My-Thoughts"
-    "Making-Windows-Usable"
-    "Prematurely-Pulling-The-Plug-On-3G"
-    "A-Year-With-The-Framework-Laptop-13"
-    "Everything-Is-Chrome"
-    "I-Hate-My-Nokia"
-    "Cybersecurity-Superstition"
-    "Minecraft-Nostalgia-And-Growing-Up"
-    "My-Code-Formatting-Guidelines"
-    "JPEG-XL-And-Googles-War-Against-It"
-    "I-Got-A-Flipper-Zero"
+  "Welcome"
+  "School-Internet"
+  "LibreOffice-Setup"
+  "Halo-My-Thoughts"
+  "Making-Windows-Usable"
+  "Prematurely-Pulling-The-Plug-On-3G"
+  "A-Year-With-The-Framework-Laptop-13"
+  "Everything-Is-Chrome"
+  "I-Hate-My-Nokia"
+  "Cybersecurity-Superstition"
+  "Minecraft-Nostalgia-And-Growing-Up"
+  "My-Code-Formatting-Guidelines"
+  "JPEG-XL-And-Googles-War-Against-It"
+  "I-Got-A-Flipper-Zero"
 )
 for feed in "${post_feeds[@]}"; do
-    adduce feed export "$feed"
+  adduce feed export "$feed"
 done
 cp -r export/. ../../docs/posts
 cd ../..
@@ -44,14 +44,14 @@ cd ../..
 # Generate portfolio items
 cd input/portfolio || exit
 portfolio_feeds=(
-    "Mutant-Remix"
-    "CapChord"
-    "Pam-Carters-Scriptural-Poetry"
-    "Photography"
-    "Meat-Typeface"
+  "Mutant-Remix"
+  "CapChord"
+  "Pam-Carters-Scriptural-Poetry"
+  "Photography"
+  "Meat-Typeface"
 )
 for feed in "${portfolio_feeds[@]}"; do
-    adduce feed export "$feed"
+	adduce feed export "$feed"
 done
 cp -r export/. ../../docs/portfolio
 cd ../..
