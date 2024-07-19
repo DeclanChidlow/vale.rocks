9a30354d8f51aec4d70936b4e6a7a786473a2205
Declan Chidlow <git.rh92c@8shield.net>
2024-07-19 09:24:05 +0800

    Update scroll indicator

--- input/global/scripts/scroll-indicator.js
+++ input/global/scripts/scroll-indicator.js
@@ -1,9 +1,9 @@
-document.getElementById("scroll-progress-container").style.display = "block";
+document.getElementById("scroll-indicator-container").style.display = "block";
 
 function addHeadingLines() {
 	const headings = document.querySelectorAll("h2, h3, h4, h5, h6"),
 		scrollableHeight = document.documentElement.scrollHeight - window.innerHeight,
-		progressContainer = document.getElementById("scroll-progress-container");
+		progressContainer = document.getElementById("scroll-indicator-container");
 
 	headings.forEach((heading) => {
 		const linePosition = (heading.offsetTop / scrollableHeight) * 100,
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", addHeadingLines);
 window.addEventListener("scroll", function () {
 	const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight,
 		scrolled = window.scrollY,
-		progressBar = document.getElementById("scroll-progress"),
+		progressBar = document.getElementById("scroll-indicator"),
 		progress = (scrolled / scrollableHeight) * 100;
 	progressBar.style.width = progress + "%";
 });
