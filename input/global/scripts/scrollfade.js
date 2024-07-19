e077a39289145b5beb58f5d9ba06aa6cc714e6ef
Declan Chidlow <git.rh92c@8shield.net>
2024-07-19 09:36:19 +0800

    Improve fading in blog posts on scroll

--- input/global/scripts/scrollfade.js
+++ input/global/scripts/scrollfade.js
@@ -1,24 +1,23 @@
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 	const elements = document.querySelectorAll(".post");
 
-	function fadeInIfInView() {
-		elements.forEach((el) => {
-			if (isInViewport(el)) {
-				el.classList.add("fade-in-up");
-			}
-		});
-	}
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("fade-in-up");
+					observer.unobserve(entry.target);
+				}
+			});
+		},
+		{
+			root: null,
+			rootMargin: "0px",
+			threshold: 0.1,
+		},
+	);
 
-	function isInViewport(element) {
-		const rect = element.getBoundingClientRect();
-		return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-	}
-
-	fadeInIfInView(); // Check initially on page load
-
-	window.addEventListener("scroll", () => {
-		elements.forEach((el) => {
-			el.classList.toggle("fade-in-up", window.scrollY >= el.offsetTop - window.innerHeight);
-		});
+	elements.forEach((el) => {
+		observer.observe(el);
 	});
 }
