dee9a0c2c961c0ca8787de765558636624b38425
Declan Chidlow <git.rh92c@8shield.net>
2024-12-10 11:47:46 +0800

    First version of bookshelf page

---
+++ src/functions/num-to-stars.js
@@ -0,0 +1,8 @@
+export default (rating) => {
+	const fullStars = Math.floor(rating);
+	const hasHalfStar = rating % 1 >= 0.5;
+	const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+	const stars = "★".repeat(fullStars) + (hasHalfStar ? "⯪" : "") + "☆".repeat(emptyStars);
+	return stars;
+};
