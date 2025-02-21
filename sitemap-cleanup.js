e0206865e487254758d31ac88b78a06c4b9901c4
Declan Chidlow <accounts@vale.rocks>
2025-02-21 19:43:01 +0800

    Add formatting to sitemap

--- sitemap-cleanup.js
+++ sitemap-cleanup.js
@@ -1,10 +1,24 @@
 const fs = require("fs");
 
-function cleanSitemap(inputPath) {
-	const sitemapContent = fs.readFileSync(inputPath, "utf8");
-	const cleanedContent = sitemapContent.replace(new RegExp(`(https://vale.rocks[^<\\s]+)(/|\.html)(?=</loc>)`, "g"), "$1");
-	fs.writeFileSync(inputPath, cleanedContent);
+function enhanceSitemap(inputPath, stylesheetUrl = "/assets/sitemap.xsl") {
+	let sitemapContent = fs.readFileSync(inputPath, "utf8");
+
+	sitemapContent = sitemapContent.replace(new RegExp(`(https://vale.rocks[^<\\s]+)(/|\.html)(?=</loc>)`, "g"), "$1");
+
+	if (!sitemapContent.includes("<?xml-stylesheet")) {
+		sitemapContent = sitemapContent.replace('<?xml version="1.0" encoding="UTF-8"?>', '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="' + stylesheetUrl + '"?>');
+	}
+
+	fs.writeFileSync(inputPath, sitemapContent);
+	console.log(`Sitemap updated with stylesheet reference: ${stylesheetUrl}`);
 }
 
 const inputPath = process.argv[2];
-cleanSitemap(inputPath);
+const stylesheetUrl = process.argv[3] || "/assets/sitemap.xsl";
+
+if (!inputPath) {
+	console.error("Error: Please provide the path to the sitemap file");
+	process.exit(1);
+}
+
+enhanceSitemap(inputPath, stylesheetUrl);
