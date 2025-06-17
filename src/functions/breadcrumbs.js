fb931391e38385e0a0a0c4927450a25649b4732b
Declan Chidlow <accounts@vale.rocks>
2025-06-17 11:59:50 +0800

    Update footer

--- src/functions/breadcrumbs.js
+++ src/functions/breadcrumbs.js
@@ -1,12 +1,16 @@
 export default (pathString) => {
 	const segments = pathString.replace(/^\//, "").split("/");
+
+	if (segments.length <= 1) {
+		return "";
+	}
+
 	let breadcrumbs = [];
 	let currentPath = "";
-
 	segments.forEach((segment) => {
 		currentPath += "/" + segment;
 		breadcrumbs.push(`<a href="${currentPath}">${segment}</a>`);
 	});
 
-	return breadcrumbs.join(" ➞ ");
+	return breadcrumbs.join("➞");
 };
