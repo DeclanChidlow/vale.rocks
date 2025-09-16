8bbf3738c52db595f3ce099186e4a88140ca9b80
Declan Chidlow <accounts@vale.rocks>
2025-09-16 08:31:34 +0800

    Simplify timezone script

--- src/assets/scripts/timezone-adjuster.js
+++ src/assets/scripts/timezone-adjuster.js
@@ -1,48 +1,18 @@
 class TimezoneConverter {
 	constructor() {
-		this.timeElements = document.querySelectorAll("time[datetime*='T'][datetime$='Z']");
-		this.init();
-	}
-
-	init() {
-		if (!this.timeElements.length) {
-			console.warn("No time elements with UTC time components found");
-			return;
-		}
-		this.convertTimezones();
-	}
-
-	formatDateTime(date) {
-		const options = {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
-			hour: "2-digit",
-			minute: "2-digit",
-			hour12: false,
-		};
-		return date.toLocaleDateString(undefined, options);
-	}
-
-	convertTimezone(timeElement) {
-		const utcDatetime = timeElement.getAttribute("datetime");
-		const date = new Date(utcDatetime);
-
-		if (isNaN(date)) {
-			console.warn(`Invalid datetime format in element: ${timeElement.outerHTML}`);
-			return;
-		}
-
-		const originalText = timeElement.textContent;
-		const localTimeText = this.formatDateTime(date);
-
-		if (originalText !== localTimeText) {
-			timeElement.textContent = localTimeText;
-		}
-	}
-
-	convertTimezones() {
-		this.timeElements.forEach((timeElement) => this.convertTimezone(timeElement));
+		document.querySelectorAll("time[datetime*='T'][datetime$='Z']").forEach((el) => {
+			const date = new Date(el.getAttribute("datetime"));
+			if (!isNaN(date)) {
+				el.textContent = date.toLocaleDateString(undefined, {
+					day: "numeric",
+					month: "short",
+					year: "numeric",
+					hour: "2-digit",
+					minute: "2-digit",
+					hour12: false,
+				});
+			}
+		});
 	}
 }
 
