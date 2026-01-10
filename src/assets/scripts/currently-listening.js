1eb5a2ea52999f412f6bd0c3b5e825349efd342e
Declan Chidlow <accounts@vale.rocks>
2026-01-10 21:54:14 +0800

    Improve music display, update micros, update library

--- src/assets/scripts/currently-listening.js
+++ src/assets/scripts/currently-listening.js
@@ -1,17 +1,27 @@
-fetch("https://api.listenbrainz.org/1/user/OuterVale/playing-now")
-	.then((r) => r.json())
-	.then((data) => {
-		const listen = data?.payload?.listens?.[0];
-		if (!listen?.playing_now) return;
+function updateListeningStatus() {
+	fetch("https://api.listenbrainz.org/1/user/OuterVale/playing-now")
+		.then((r) => r.json())
+		.then((data) => {
+			const listen = data?.payload?.listens?.[0];
+			const meta = listen?.track_metadata;
+			let nextCheck = 30000;
 
-		const { track_name, artist_name, release_name } = listen.track_metadata || {};
-		const msg = [
-			track_name ? `<em id="song">${track_name}</em>` : null,
-			artist_name ? `<em id="artist">${artist_name}</em>` : null,
-			release_name ? `<em id="album">${release_name}</em>` : null,
-		].filter(Boolean);
+			if (listen?.playing_now && meta) {
+				const { track_name, artist_name, release_name, additional_info } = meta;
 
-		document.getElementById("currently-listening").innerHTML =
-			`At this very moment I’m listening to ${msg[0] || msg[1] || msg[2]}${msg[1] ? ` by ${msg[1]}` : ""}${msg[2] ? ` from the album ${msg[2]}` : ""}.`;
-	})
-	.catch(console.error);
+				document.getElementById("currently-listening").innerHTML =
+					`At this very moment I’m listening to <em>${track_name}</em>${artist_name ? ` by <em id="artist">${artist_name}</em>` : ""}${release_name ? ` from the album <em id="album">${release_name}</em>` : ""}.`;
+
+				if (additional_info?.duration) {
+					nextCheck = additional_info.duration * 1000 + 2500;
+				}
+			} else {
+				document.getElementById("currently-listening").innerHTML = "";
+			}
+
+			setTimeout(updateListeningStatus, nextCheck);
+		})
+		.catch(() => setTimeout(updateListeningStatus, 30000));
+}
+
+updateListeningStatus();
