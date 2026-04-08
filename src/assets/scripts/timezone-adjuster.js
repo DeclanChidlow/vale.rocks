74c874417c205fdc2e1d620e70af1e130b3a9240
Declan Chidlow <accounts@vale.rocks>
2026-04-08 10:18:54 +0800

    Dynamic timezone labels

--- src/assets/scripts/timezone-adjuster.js
+++ src/assets/scripts/timezone-adjuster.js
@@ -1,21 +1,36 @@
 class TimezoneConverter {
 	constructor() {
-		const formatter = new Intl.DateTimeFormat(undefined, {
+		const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
 			day: "numeric",
 			month: "short",
 			year: "numeric",
 			hour: "2-digit",
 			minute: "2-digit",
 			hour12: false,
+			timeZoneName: "short",
 		});
 
-		const timeElements = document.querySelectorAll("time[datetime*='T'][datetime$='Z']");
+		const dateFormatter = new Intl.DateTimeFormat(undefined, {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+			timeZoneName: "short",
+		});
+
+		const timeElements = document.querySelectorAll("time[datetime]:not([data-timezone-ignore])");
 
 		timeElements.forEach((el) => {
 			const date = new Date(el.getAttribute("datetime"));
 
 			if (!isNaN(date)) {
-				el.textContent = formatter.format(date);
+				const originalText = el.textContent;
+				const hasTime = originalText.includes(":") || originalText.includes(",");
+
+				if (hasTime) {
+					el.textContent = dateTimeFormatter.format(date);
+				} else {
+					el.textContent = dateFormatter.format(date);
+				}
 			}
 		});
 	}
