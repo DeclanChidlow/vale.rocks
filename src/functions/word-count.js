731c95bdac5cdca17cb16cd4ab581cade870cb1f
Declan Chidlow <accounts@vale.rocks>
2026-04-09 13:39:56 +0800

    Improve accuracy of word counts

--- src/functions/word-count.js
+++ src/functions/word-count.js
@@ -1,4 +1,12 @@
-export default (text) => {
-	const textWithoutExclusions = text.replace(/<pre[\s\S]*?<\/pre>/gi, "").replace(/<svg[\s\S]*?<\/svg>/gi, "");
-	return textWithoutExclusions.split(" ").filter((w) => w.trim()).length;
+export default (text, locale = "en-AU") => {
+	const cleanText = text.replace(/<(script|style|noscript|pre|svg)[\s\S]*?<\/\1>/gi, "");
+	const segmenter = new Intl.Segmenter(locale, { granularity: "word" });
+	const segments = segmenter.segment(cleanText);
+
+	let count = 0;
+	for (const segment of segments) {
+		if (segment.isWordLike) count++;
+	}
+
+	return count;
 };
